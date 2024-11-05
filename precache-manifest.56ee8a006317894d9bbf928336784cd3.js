a5b944257a7c0f5716c5c55aa4ade20f71cca168
Zak <zak@MacBook-Air-de-Zak.local>
2024-11-05 10:02:12 +0100

    Updates

---
+++ precache-manifest.56ee8a006317894d9bbf928336784cd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36059bc19ec192897e9b23e0843d75b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b88fbb5729319aceeee",
+    "url": "/static/css/main.65d82c09.chunk.css"
+  },
+  {
+    "revision": "5862057875a5ca34d9bf",
+    "url": "/static/js/2.9e328a3e.chunk.js"
+  },
+  {
+    "revision": "a36385f465a2b6f1dd2c8470ea9178b8",
+    "url": "/static/js/2.9e328a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a55cc8fd597a96698a1",
+    "url": "/static/js/3.4cc05082.chunk.js"
+  },
+  {
+    "revision": "8b88fbb5729319aceeee",
+    "url": "/static/js/main.409c11bd.chunk.js"
+  },
+  {
+    "revision": "4bb47469dc1f399cf00f",
+    "url": "/static/js/runtime-main.9dede2b3.js"
+  }
+]);
